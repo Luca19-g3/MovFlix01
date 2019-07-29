d4dca4049705e7d7071b4eda2873cf99350670d1
spicordova <spicordova@gmail.com>
2019-07-29 15:10:13 +0200

    he creado el metodo altausuario en la clase servicios y he añadido opciones al menu

--- src/gui/Menu.java
+++ src/gui/Menu.java
@@ -13,6 +13,13 @@ public class Menu {
 		System.out.println(" ");
 		System.out.println("************************************************");
 		System.out.println("1. Alta pelicula");
+		System.out.println("2. Modificar pelicula");
+		System.out.println("3. Baja pelicula");
+		System.out.println("4. Listar peliculas");
+		System.out.println("5. Alta usuario");
+		System.out.println("6. Modificar Usuario");
+		System.out.println("7. Baja Usuario");
+		System.out.println("8. Listar Usuarios");
 		
 	}
 }
