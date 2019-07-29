f80d2520cfaf8d470dddd2478a091b58022954a0
spicordova <spicordova@gmail.com>
2019-07-29 13:29:42 +0200

    He creado la clase pelicula con el metodo crearPelicula() y he agregado comentarios JavaDoc en Datos

---
+++ src/modelo/Pelicula.java
@@ -0,0 +1,83 @@
+package modelo;
+
+import utilidades.Datos;
+
+public class Pelicula {
+
+	private int id_pelicula;
+	private String nombre;
+	private int año_estreno;
+	private String categoria;
+
+	public Pelicula() {
+		super();
+	}
+
+	public Pelicula(int id_pelicula, String nombre, int año_estreno, String categoria) {
+		super();
+		this.id_pelicula = id_pelicula;
+		this.nombre = nombre;
+		this.año_estreno = año_estreno;
+		this.categoria = categoria;
+	}
+
+	public int getId_pelicula() {
+		return id_pelicula;
+	}
+
+	public void setId_pelicula(int id_pelicula) {
+		this.id_pelicula = id_pelicula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getAño_estreno() {
+		return año_estreno;
+	}
+
+	public void setAño_estreno(int año_estreno) {
+		this.año_estreno = año_estreno;
+	}
+
+	public String getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	/**
+	 * este metodo pide nombre, categoria y año de estreno para crear objetos de la
+	 * clase pelicula
+	 * 
+	 * @param none 
+	 * @return void
+	 * 
+	 */
+
+	public void crearPelicula() {
+		try {
+
+			this.nombre = Datos.recogeString("Introduce el nombre de la pelicula: ");
+			this.año_estreno = Datos.recogeInt("Introduce el año de estreno: ");
+			this.categoria = Datos.recogeString("Introduzca la categoria de la pelicula: ");
+
+		} catch (Exception e) {
+			e.getStackTrace();
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Pelicula [id_pelicula=" + id_pelicula + ", nombre=" + nombre + ", año_estreno=" + año_estreno
+				+ ", categoria=" + categoria + "]";
+	}
+
+}
--- src/utilidades/Datos.java
+++ src/utilidades/Datos.java
@@ -3,42 +3,77 @@ package utilidades;
 import java.util.Scanner;
 
 public class Datos {
-	
-public Datos() {
-		
+
+	public Datos() {
+
 	}
-	 @SuppressWarnings("resource")
-		public static String recogeString() throws Exception {
-			String dato = new Scanner(System.in).nextLine();
-	        return dato;
-	    }
 
-	    public static String recogeString(String msg) throws Exception {
-	        System.out.println(msg);
-	        return recogeString();
-	    }
+	/**
+	 * @param
+	 * @return String
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lectura y recogida de datos.
+	 */
+	@SuppressWarnings("resource")
+	public static String recogeString() throws Exception {
+		String dato = new Scanner(System.in).nextLine();
+		return dato;
+	}
+	/**
+	 * @param	String mensaje que se muestra al pedir los datos
+	 * @return String
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lecturay recogida de datos.
+	 */
+	public static String recogeString(String msg) throws Exception {
+		System.out.println(msg);
+		return recogeString();
+	}
 
-	    @SuppressWarnings("resource")
-	    public static int recogeInt() throws Exception {
-	        int dato = new Scanner(System.in).nextInt();
-	        return dato;
-	    }
+	/**
+	 * @param
+	 * @return Int
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lectura y recogida de datos.
+	 */
+	@SuppressWarnings("resource")
+	public static int recogeInt() throws Exception {
+		int dato = new Scanner(System.in).nextInt();
+		return dato;
+	}
 
-	    public static int recogeInt(String msg) throws Exception {
-	        System.out.println(msg);
-	        return recogeInt();
-	    }
+	/**
+	 * @param	String mensaje que se muestra al pedir los datos
+	 * @return Int
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lecturay recogida de datos.
+	 */
+	public static int recogeInt(String msg) throws Exception {
+		System.out.println(msg);
+		return recogeInt();
+	}
 
-	    @SuppressWarnings("resource")
-	    public static double recogeDouble() throws Exception {
-	        double dato = new Scanner(System.in).nextDouble();
-	        return dato;
-	    }
+	/**
+	 * @param
+	 * @return double
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lectura y recogida de datos.
+	 */
+	@SuppressWarnings("resource")
+	public static double recogeDouble() throws Exception {
+		double dato = new Scanner(System.in).nextDouble();
+		return dato;
+	}
 
-	    public static double recogeDouble(String msg) throws Exception {
-	        System.out.println(msg);
-	        return recogeDouble();
-	    }
-
+	/**
+	 * @param	String mensaje que se muestra al pedir los datos
+	 * @return Double
+	 * @throws Exception 
+	 * Estos metodos se utilizan para la lecturay recogida de datos.
+	 */
+	public static double recogeDouble(String msg) throws Exception {
+		System.out.println(msg);
+		return recogeDouble();
+	}
 
 }
