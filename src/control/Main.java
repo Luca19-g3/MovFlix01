2c02b8016dcf074c7bf2a5082f68ffb714d74832
Edmund Home <edmundhome@hotmail.com>
2019-07-29 17:39:56 +0200

    dfasd

--- src/control/Main.java
+++ src/control/Main.java
@@ -4,22 +4,14 @@ import datos.PeliculaDao;
 import datos.UsuarioDao;
 import modelo.Pelicula;
 import modelo.Usuario;
+import vistas.VistaPelicula;
+import vistas.VistaUsuario;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		PeliculaDao.altaPelicula(new Pelicula ("Torrente 6", 2020, "3"));
-		
-		for (Pelicula p : PeliculaDao.listarPelicula()) {
-			System.out.println(p.toString());
-		}
-
-		for (Usuario u : UsuarioDao.listarUsuario()) {
-			System.out.println(u.toString());
-		}
-
-	
+ VistaUsuario.mostrarListaUsuarios(UsuarioDao.listarUsuario());
 
 		
 	}
