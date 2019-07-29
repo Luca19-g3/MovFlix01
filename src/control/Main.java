7f547a4f73082baec6570000f7493a74eb896758
Edmund Home <edmundhome@hotmail.com>
2019-07-29 16:40:42 +0200

    arreglar clase

--- src/control/Main.java
+++ src/control/Main.java
@@ -7,6 +7,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		PeliculaDao.altaPelicula(new Pelicula ("Torrente 6", 2020, "3"));
-		
+		PeliculaDao.listarPelicula();
 	}
 }
--- src/datos/PeliculaDao.java
+++ src/datos/PeliculaDao.java
@@ -16,57 +16,49 @@ public class PeliculaDao {
 	static Conexion con = new Conexion();
 
 	public static void altaPelicula(Pelicula pelicula) {
-<<<<<<< HEAD
-		String sql = "INSERT INTO Peliculas (Nombre ,Ano_Estreno ,id_categorias, id_paquetes) values ('"+pelicula.getNombre()+"',"pelicula.getAno_estreno());
-		System.out.println(sql);
-=======
-		
-		String sql = "INSERT INTO cliente values (NULL,'" + pelicula.getNombre() + "','" + pelicula.getAño_estreno()
+
+
+
+		String sql = "INSERT INTO cliente values (NULL,'" + pelicula.getNombre() + "','" + pelicula.getAnho_estreno()
 				+ "','" + pelicula.getCategoria() + "')";
->>>>>>> 4c68cd8d7b597425804f719852428c4d571ea738
+
 
 		try {
 
 			st = con.getConnection().createStatement();
 			st.executeUpdate(sql);
 
-<<<<<<< HEAD
-			ps.setString(2, "Torrente 7");
-			ps.setString(3, "2020");
-			ps.setInt(4, 1);
 
-			ps.executeUpdate();
 
-		} catch (
-=======
-<<<<<<< HEAD
+
 		
-=======
+
 			while (rs.next()) {
 //Aqui de momento nose
-			}
->>>>>>> 3260995b15e12e951704b1bc481cf47ae3a6a1c4
-		} catch (SQLException ex) {
->>>>>>> 4c68cd8d7b597425804f719852428c4d571ea738
+			
+		}
 
-		SQLException ex) {
-			System.out.println(ex);
+		} catch (SQLException ex) {
+
+
+
 		}
 	}
 
-	/*
-	 * public static void listarPelicula() {
-	 * 
-	 * try { st = con.getConnection().createStatement(); rs =
-	 * st.executeQuery("select * from Peliculas");
-	 * 
-	 * while (rs.next()) { System.out.print(rs.getString(1) + "-");
-	 * System.out.print(rs.getString(2) + "-"); System.out.println(rs.getString(3));
-	 * 
-	 * } }catch (SQLException ex) {
-	 * 
-	 * } }
-	 */
+
+	
+	  public static void listarPelicula() {
+	  
+	  try { st = con.getConnection().createStatement(); rs =
+	  st.executeQuery("select * from Peliculas");
+	  
+	  while (rs.next()) { System.out.print(rs.getString(1) + "-");
+	  System.out.print(rs.getString(2) + "-"); System.out.println(rs.getString(3));
+	  
+	 } }catch (SQLException ex) {
+	 
+	  } }
+	 
 
 	public static void modificarPelicula() {
 
