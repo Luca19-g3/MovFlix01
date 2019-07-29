0f2f3d6fa9a5812d27b7e71c3e2a3555ee09ea78
Edmund Home <edmundhome@hotmail.com>
2019-07-29 12:26:28 +0200

    Hola

--- src/control/Main.java
+++ src/control/Main.java
@@ -3,8 +3,10 @@ package control;
 public class Main {
 
 	public static void main(String[] args) {
-System.out.print("prueba");
-System.out.print("Prueba 2");
-	System.out.println("Prueba 3");}
+		System.out.print("prueba");
+		System.out.print("Prueba 2");
+		System.out.println("Prueba 3");
+		System.out.println("Puto GITHUB");
 
+	}
 }
