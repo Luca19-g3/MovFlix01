f0e656475c2e59fc9d600499f36e178d158ce7f1
Edmund Home <edmundhome@hotmail.com>
2019-07-30 16:54:19 +0200

    Peliculas mas valoradas

--- src/datos/IPeliculasDao.java
+++ src/datos/IPeliculasDao.java
@@ -6,7 +6,7 @@ import modelo.Pelicula;
 public interface IPeliculasDao {
 	public  void altaPelicula(Pelicula p);
 	public List<Pelicula> obtenerPeliculas();
-	public List<Pelicula> obtenerPeliculas(String flitro);
+	public List<Pelicula> obtenerPeliculas(String filtro);
 	public List<Pelicula> obtenerPeliculas(String filtro, int nresultados);
 	public boolean modificarPelicula(Pelicula p, int id);
 	public boolean bajaPelicula(int id);
