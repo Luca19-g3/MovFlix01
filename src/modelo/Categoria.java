9994c7fcfb603fa52b483b6bb8395787a8f2ebb1
spicordova <spicordova@gmail.com>
2019-07-30 10:40:49 +0200

    He modificado la calse categoria

--- src/modelo/Categoria.java
+++ src/modelo/Categoria.java
@@ -33,8 +33,8 @@ public class Categoria {
      */
     public void crearCategoria() {
         try {
-            this.idcategoria = Datos.recogeInt("Digite el id de la categoría");
-            this.nombre = Datos.recogeString("Digite el nombre de la categoría");
+            this.idcategoria = Datos.recogeInt("Digite el id de la categoria");
+            this.nombre = Datos.recogeString("Digite el nombre de la categoria");
         } catch (Exception e) {
             e.getStackTrace();
         }
