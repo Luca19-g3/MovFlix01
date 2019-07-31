9f54cd6c92dab9aad65213801954372acca7d13d
Luca19-g3 <53351335+Luca19-g3@users.noreply.github.com>
2019-07-31 10:51:36 +0200

    Update IPeliculasDao.java

--- src/datos/IPeliculasDao.java
+++ src/datos/IPeliculasDao.java
@@ -1,3 +1,11 @@
+/**
+ * Clase IPeliculasDao
+ *Interfaz que contiene los metodos para la clase PeliculasDao
+ *@author grupo3
+ *@version 1.0
+ * fecha 31/07/2019
+ * 
+ */
 package datos;
 
 import java.util.List;
